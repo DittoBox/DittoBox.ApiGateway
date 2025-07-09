84089e85a566295cd9808d4e9076afd4362ec554
dcancho <d.cancho.c@gmail.com>
2025-07-09 04:29:03 -0500

    task: add cors allowall

--- DittoBox.ApiGateway/Program.cs
+++ DittoBox.ApiGateway/Program.cs
@@ -1,16 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add YARP and load config from appsettings
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// Add YARP from config
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
+// Order matters!
+app.UseCors("AllowAll"); // Debe ir antes de MapReverseProxy
 
 app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }));
 
-// Enable routing via YARP
 app.MapReverseProxy();
 
-app.UseCors("AllowAll");
 app.Run();
